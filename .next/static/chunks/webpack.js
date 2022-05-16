3bb16c1ce98bd53346d1aef68b34d3ac9a9b484b
Elenaaab <ellelaby@gmail.com>
2022-05-16 11:58:15 +0200

    Style components row & homepage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "da36bb957e413d76"; }
+/******/ 		__webpack_require__.h = function() { return "a8ea3818b4782a2e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Movie } from '../types'
 
 interface Props {
-  movie: Movie
+  movie: Movie // | DocumentData
 }
 function Thumbnail({ movie }:Props) {
   return (
